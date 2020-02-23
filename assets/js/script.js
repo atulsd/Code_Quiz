6afd6f91b0cb1d123c8bb5058c42b90459d1740a
Atul Mahajan <luta_atul@hotmail.com>
2020-02-23 11:42:04 +1100

    Added object for questions and answers.

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// var questions = ["a", "b", "c"];
+// var answersA = ["1a", "2a", "3a", "4a"];
+
+var qandA = {
+  first: {
+    question: "a",
+    answer1: "1a",
+    answer2: "2a",
+    answer3: "3a",
+    answer4: "4a"
+  },
+  second: {
+    question: "b",
+    answer1: "1b",
+    answer2: "2b",
+    answer3: "3b",
+    answer4: "4b"
+  },
+  third: {
+    question: "c",
+    answer1: "1c",
+    answer2: "2c",
+    answer3: "3c",
+    answer4: "4c"
+  }
+};
