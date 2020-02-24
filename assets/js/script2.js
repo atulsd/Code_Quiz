a11963aa0dac2ee82594fed429fc60292befba9f
Atul Mahajan <luta_atul@hotmail.com>
2020-02-25 00:22:44 +1100

    Set time for displaying result of last question.

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,6 +3,12 @@ var resultIs = localStorage.getItem("finalResult");
 var highScore = document.querySelector("#highscore");
 
 var score = localStorage.getItem("score");
-
 result.textContent = resultIs;
+
+var resultTime = setInterval(function() {
+  result.textContent = "";
+}, 1000);
+
+//result();
+
 highScore.textContent = score;
