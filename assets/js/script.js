95b5cfa0ea55c2e1506b6568cbb9f6b5e3421ff7
Atul Mahajan <luta_atul@hotmail.com>
2020-02-24 23:25:40 +1100

    Removed unwanted code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,30 +14,6 @@ var viewInitials;
 
 var startQuizcount = 0;
 
-var qandA = {
-  first: {
-    question: "a",
-    answer1: "1a",
-    answer2: "2a",
-    answer3: "3a",
-    answer4: "4a"
-  },
-  second: {
-    question: "b",
-    answer1: "1b",
-    answer2: "2b",
-    answer3: "3b",
-    answer4: "4b"
-  },
-  third: {
-    question: "c",
-    answer1: "1c",
-    answer2: "2c",
-    answer3: "3c",
-    answer4: "4c"
-  }
-};
-
 // setTime();
 // displayQuestion();
 // checkAnswer();
@@ -51,13 +27,8 @@ var qandA = {
 // viewInitials();
 
 localStorage.setItem("startQuizcount", startQuizcount);
-//startQuizcount = localStorage.getItem("startQuizcount");
-//alert("From local storage." + startQuizcount);
-//if (startQuizcount === "1") {
-//  alert("Start quiz count is:" + startQuizcount);
 startQuiz.addEventListener("click", function(event) {
   event.preventDefault();
-  //  localStorage.setItem("startQuizcount", startQuizcount);
   location.href = "questions.html";
   alert("Start quiz count is:" + startQuizcount);
 });
