aa39616e6aed75caf47e662e164902c2bff7802b
Atul Mahajan <luta_atul@hotmail.com>
2020-02-26 00:44:03 +1100

    Stored time in local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,5 +29,6 @@ var startQuizcount = 0;
 localStorage.setItem("startQuizcount", startQuizcount);
 startQuiz.addEventListener("click", function(event) {
   event.preventDefault();
+  localStorage.setItem("time", 100);
   location.href = "questions.html";
 });
