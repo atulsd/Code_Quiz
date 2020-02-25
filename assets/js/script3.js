54a467ab8f7ed37737c91c897990aec20f5b77ae
Atul Mahajan <luta_atul@hotmail.com>
2020-02-26 00:08:34 +1100

    Added list of High Scorer with Initials and Score.

---
+++ assets/js/script3.js
@@ -0,0 +1,29 @@
+var resultIs = localStorage.getItem("score");
+var initials = localStorage.getItem("initials");
+
+var list = document.querySelector("#list");
+
+var initialsAndscore = document.createElement("li");
+initialsAndscore.innerText = initials + " " + resultIs;
+
+list.appendChild(initialsAndscore);
+
+var goBack = document.querySelector("#add-btn");
+
+$("#add-btn").on("click", function() {
+  sendToindex();
+});
+
+$("#add-btn1").on("click", function() {
+  clearHighscores();
+});
+
+function clearHighscores() {
+  event.preventDefault();
+  list.remove();
+}
+
+function sendToindex() {
+  event.preventDefault();
+  location.href = "index.html";
+}
