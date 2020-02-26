c18e23b136f02416787ff689e761e32314d4978a
Atul Mahajan <luta_atul@hotmail.com>
2020-02-26 21:53:15 +1100

    Added real questions and removed testing data.

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -3,11 +3,40 @@ var question = document.querySelector("#question");
 var timeStart = localStorage.getItem("time");
 
 var timer = document.querySelector("#timer");
-var questions = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
-var answersQone = ["1a", "1b", "1c", "1d"];
-var answersQtwo = ["2a", "2b", "2c", "2d"];
-var answersQthree = ["3a", "3b", "3c", "3d"];
-var correctAnswers = ["1a", "2b", "3c"];
+var questions = [
+  "What does Nan means?",
+  "What will the code below output? console.log(0.1 + 0.2) console.log(0.1 + 0.2 == 0.3);",
+  "In what order will the numbers 1-4 be logged to the console when the code below is executed? Why?(function() {console.log(1); setTimeout(function(){console.log(2)}, 1000); setTimeout(function(){console.log(3)}, 0); console.log(4);})();",
+  "Consider the following code snippet:for (var i = 0; i < 5; i++) {var btn = document.createElement('button');btn.appendChild(document.createTextNode('Button ' + i));btn.addEventListener('click', function(){ console.log(i); });document.body.appendChild(btn);}(a) What gets logged to the console when the user clicks on “Button 4” and why?",
+  "What will the code below output to the console and why?var arr1 = 'john'.split('');var arr2 = arr1.reverse();var arr3 = 'jones'.split('');arr2.push(arr3);console.log('array 1: length=' + arr1.length + ' last=' + arr1.slice(-1));console.log('array 2: length=' + arr2.length + ' last=' + arr2.slice(-1));"
+];
+var answersQone = [
+  "Not a number",
+  "Not a String",
+  "Not a boolean",
+  "Not a null"
+];
+var answersQtwo = [
+  "0.3 true",
+  "0.30000000000000004 false",
+  "0.3 not equal",
+  "0.3 string"
+];
+var answersQthree = ["1,3,4,2", "1,2,3,4", "1,4,3,2", "3,1,4,2"];
+var answersQfour = ["2", "4", "3", "5"];
+var answersQfive = [
+  "array 1: length=5 last=j,o,n,e,s and array 2: length=5 last=j,o,n,e,s",
+  "array 1: length=6 last=j,o,n,e,s and array 2: length=5 last=j,o,n,e,s",
+  "array 1: length=4 last=j,o,n,e,s and array 2: length=5 last=j,o,n,e,s",
+  "array 1: length=3 last=j,o,n,e,s and array 2: length=5 last=j,o,n,e,s"
+];
+var correctAnswers = [
+  "Not a number",
+  "0.30000000000000004 false",
+  "1,4,3,2",
+  "5",
+  "array 1: length=5 last=j,o,n,e,s and array 2: length=5 last=j,o,n,e,s"
+];
 
 var answers;
 var stored;
@@ -51,6 +80,14 @@ function addAnswers() {
     question.textContent = questions[parseInt(stored)];
     answers = answersQthree;
     localStorage.setItem("startQuizcount", "3");
+  } else if (stored === "3") {
+    question.textContent = questions[parseInt(stored)];
+    answers = answersQfour;
+    localStorage.setItem("startQuizcount", "4");
+  } else if (stored === "4") {
+    question.textContent = questions[parseInt(stored)];
+    answers = answersQfive;
+    localStorage.setItem("startQuizcount", "5");
   } else {
     localStorage.setItem("score", score);
     location.href = "highscores.html";
@@ -70,14 +107,19 @@ function addAnswers() {
 answer.addEventListener("click", function(event) {
   var element = event.target;
   var index = element.getAttribute("data-index");
-  if (answers[index] === correctAnswers[index]) {
-    result.textContent = "Correct answer" + answers[index];
-    var resultTime = setInterval(function() {
-      result.textContent = "";
-    }, 3000);
-    localStorage.setItem("finalResult", "Correct");
-    score += 10;
-  } else {
+  var found = 0;
+  for (var i = 0; i < correctAnswers.length; i++) {
+    if (answers[index] === correctAnswers[i]) {
+      result.textContent = "Correct answer" + answers[index];
+      var resultTime = setInterval(function() {
+        result.textContent = "";
+      }, 3000);
+      localStorage.setItem("finalResult", "Correct");
+      score += 10;
+      found = 1;
+    }
+  }
+  if (found === 0) {
     result.textContent = "Incorrect answer" + answers[index];
     var resultTime = setInterval(function() {
       result.textContent = "";
