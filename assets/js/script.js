6086ccb2a763e7b3dd06d0584c46deb29b67ee9c
Atul Mahajan <luta_atul@hotmail.com>
2020-03-05 23:51:08 +1100

    Cleaned the file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,5 @@
 var startQuiz = document.querySelector("#startQuiz");
-var displayQuestion;
-//var checkAnswer = document.querySelector("#data-answer");
-var displayAnswer;
-var generateScore;
-var subtractTimer;
-var setTime;
-var timeRemaining;
-var gameOver;
-var addTotalscore;
-var viewHighscores;
-var addInitials;
-var viewInitials;
-
 var startQuizcount = 0;
-
-// setTime();
-// displayQuestion();
-// checkAnswer();
-// displayAnswer();
-// subtractTimer();
-// generateScore();
-// addTotalscore();
-// gameOver();
-// addInitials();
-// viewHighscores();
-// viewInitials();
 
 localStorage.setItem("startQuizcount", startQuizcount);
 startQuiz.addEventListener("click", function(event) {
